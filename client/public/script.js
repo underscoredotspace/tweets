286f80a57c67fa46faa0d56c7414c15e2bbe0a86
ampersand <ampersand@underscore.space>
2017-01-18 00:01:37 +0000

    Initial tweet column created Dummy data and service for getting tweets Tweet column requesting more tweets when fewer than 5 tweets are below scroll

--- client/public/script.js
+++ client/public/script.js
@@ -1,1 +1,29 @@
-angular.module('tweetsApp', ['ngCleanToast', 'ngIsVisible']);
+angular.module('tweetsApp', ['ngCleanToast', 'ngIsVisible', 'ngTweetomatic'])
+
+.controller('tweetsController', function($scope, tweets, toasts) {
+    $scope.tweets = [];
+    $scope.gettweets = function() {
+        newTweets = tweets.get($scope.tweets.length, 20);
+        if (newTweets.length >0) {
+            newTweets.forEach(function(newTweet) {
+                $scope.tweets.push(newTweet);
+            });
+        }
+    }
+    $scope.gettweets();
+})
+
+.directive('tweet', function() {
+    return {
+        restrict: 'C',
+        link: function(scope, element, attr) {
+            scope.$on('visible', function(e, visible) {
+                wasVisible = scope.visible;
+                scope.visible = visible;
+                if ((visible) && (!wasVisible) && (scope.$parent.tweets.indexOf(scope.tweet) == scope.$parent.tweets.length - 5)) {
+                    scope.$parent.gettweets();
+                }
+            });
+        }
+    }
+});
